e670c9dbc3da30bd6c6dc9b81712fb5cd60f75ca
danielmestre98 <dmestreloureiro@gmail.com>
2023-08-17 13:40:17 -0300

    Finalizado página inical da intranet

--- src/components/Home/RightColumn.jsx
+++ src/components/Home/RightColumn.jsx
@@ -1,9 +1,29 @@
+import { useRouter } from "next/navigation";
 import CardIntranet from "../Card/Card";
 import Agenda from "./Agenda/Agenda";
 import Note from "./Note/Note";
-import { HomeDiv } from "./styles";
+import { HomeDiv, ImageBanner } from "./styles";
+
+function isValidURL(str) {
+    try {
+        new URL(str);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
 
 const RightColumn = () => {
+    const router = useRouter();
+
+    const handleBannerClick = (link, toBlank) => {
+        if (isValidURL(link) || toBlank === true) {
+            window.open(link);
+        } else {
+            router.push(link);
+        }
+    };
+
     return (
         <HomeDiv>
             <CardIntranet cardBodyStyle={{ padding: "0px" }} style={{ height: "400px" }}>
@@ -11,6 +31,20 @@ const RightColumn = () => {
             </CardIntranet>
             <CardIntranet cardTitle="Recados Importantes" style={{ height: "674.5px" }}>
                 <Note />
+            </CardIntranet>
+            <CardIntranet>
+                <ImageBanner onClick={() => handleBannerClick("https://treinamentos.spsempapel.sp.gov.br/")}>
+                    <img src="/img/home-banners/banner_sp_sem_papel.jpg" alt="..." />
+                </ImageBanner>
+                <ImageBanner
+                    onClick={() =>
+                        handleBannerClick("/documents/home/CARTILHA VOLTA AO TRABALHO PRESENCIAL.pdf", true)
+                    }>
+                    <img src="/img/home-banners/banner_cartilha.png" alt="..." />
+                </ImageBanner>
+                <ImageBanner onClick={() => handleBannerClick("/edesp")}>
+                    <img src="/img/home-banners/banner_edesp.png" alt="..." />
+                </ImageBanner>
             </CardIntranet>
         </HomeDiv>
     );
