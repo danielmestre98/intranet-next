f882c8c067202b809c42f7c5c8bf1d59218e29bb
danielmestre98 <dmestreloureiro@gmail.com>
2024-07-02 13:23:36 -0300

    Importação de bibliotecas que faltaram

--- src/app/(private)/logout/page.js
+++ src/app/(private)/logout/page.js
@@ -1,8 +1,12 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 const Logout = () => {
+    const router = useRouter();
+    const dispatch = useDispatch();
     useEffect(() => {
         dispatch(logoutUser());
         router.push("/login");
